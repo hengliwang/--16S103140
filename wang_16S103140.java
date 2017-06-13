f2a890990088749da133004e07ea8b81d147522e
hengliwang <1126050916@qq.com>
2017-06-13 10:26:43 +0800

    Add files via upload

---
+++ wang_16S103140.java
@@ -0,0 +1,10 @@
+package github_submit;
+
+public class wang_16S103140 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("我是王恒立，学号16S103140！！！");
+	}
+
+}
